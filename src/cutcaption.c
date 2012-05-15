3aff3f8edad252736eec97a03fba64a2d78da641
Masaki Tanaka <maki.rxrz@gmail.com>
2012-05-15 23:46:44 +0900

    1.0.2

--- src/cutcaption.c
+++ src/cutcaption.c
@@ -45,7 +45,7 @@
 #include "avs_utils.h"
 #include "mpegts_utils.h"
 
-#define PROGRAM_VERSION                 "1.0.1"
+#define PROGRAM_VERSION                 "1.0.2"
 
 #ifndef REVISION_NUMBER
 #define REVISION_NUMBER                 "0"
