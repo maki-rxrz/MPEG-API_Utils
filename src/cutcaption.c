4979f104f4ff8820a7340d180e5c3f2676a724e0
Masaki Tanaka <maki.rxrz@gmail.com>
2019-09-21 12:07:42 +0900

    utils: CutCaption 1.2.4

--- src/cutcaption.c
+++ src/cutcaption.c
@@ -41,7 +41,7 @@
 #include "d2v_parser.h"
 #include "mpeg_utils.h"
 
-#define PROGRAM_VERSION                 "1.2.3"
+#define PROGRAM_VERSION                 "1.2.4"
 
 #ifndef REVISION_NUMBER
 #define REVISION_NUMBER                 "0"
