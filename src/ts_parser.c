869f226f00ccedff79620431531cf876d979488d
Masaki Tanaka <maki.rxrz@gmail.com>
2023-11-03 00:00:00 +0900

    utils: MPEG-2 TS/ES Parser 0.2.2

--- src/ts_parser.c
+++ src/ts_parser.c
@@ -44,7 +44,7 @@
 
 #define PROGRAM_NAME                    "MPEG-2 TS/ES Parser"
 
-#define PROGRAM_VERSION                 "0.2.1"
+#define PROGRAM_VERSION                 "0.2.2"
 
 #ifndef REVISION_NUMBER
 #define REVISION_NUMBER                 "0"
